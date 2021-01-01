cdafb435efb1e53cac565e6a63daf6c54d7ab2de
Elijah Simpson <elijahsimpson1991@gmail.com>
2020-12-31 23:19:52 -0500

    added game total on console card component

--- src/components/ConsoleCard/ConsoleCard.jsx
+++ src/components/ConsoleCard/ConsoleCard.jsx
@@ -6,7 +6,7 @@ import Main from "../../api/Main";
 import { useHistory } from "react-router-dom";
 
 export default function ConsoleCard(props) {
-  const { deleteConsole } = useContext(GamestashContext);
+  const { deleteConsole, games } = useContext(GamestashContext);
   let history = useHistory();
 
   const handleDelete = async (id) => {
@@ -20,9 +20,11 @@ export default function ConsoleCard(props) {
     history.push(`/consoles/${id}`);
   };
 
+  const gameTotal = games.filter((game) => game.console_id === props.id)
+
   return (
     <div className="console-card">
-      <span>{props.name}</span>
+      <span>{props.name} | Game Total:{gameTotal.length}</span>
       <br />
       <button area-label="Games" onClick={() => handleConsoleSelect(props.id)}>Games</button>
       <button area-aria-label="Delete" onClick={() => handleDelete(props.id)}>Delete</button>
